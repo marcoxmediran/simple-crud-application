aa19ec9ce212c3236fa974e515ddf7de755adbae
marcoxmediran <marcoxmediran@gmail.com>
2025-07-15 15:05:36 +0800

    chore: create new root/home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <div>
+        <p>Hello, world!</p>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', '/home');
+
+Route::get('/home', function () {
+    return view('home');
 });
