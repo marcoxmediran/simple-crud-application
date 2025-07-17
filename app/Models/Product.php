540a9a0df748ae080d13acbd791b87343897cab5
marcoxmediran <marcoxmediran@gmail.com>
2025-07-17 12:52:03 +0800

    feat: create product model and controller

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,10 +8,17 @@ class Product extends Model
 {
     protected $table = 'tblproducts';
 
+    protected $primaryKey = 'prd_id';
+
     protected $fillable = [
         'prd_name',
         'prd_description',
         'prd_quantity',
         'prd_price',
     ];
+
+    public function getRouteKeyName()
+    {
+        return 'prd_id';
+    }
 }
