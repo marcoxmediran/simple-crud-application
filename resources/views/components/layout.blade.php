f0bf0d5dddbf7a5228db353a1685b3547928a00e
marcoxmediran <marcoxmediran@gmail.com>
2025-07-17 12:53:25 +0800

    chore: remove CDN fonts, set website title

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,11 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title></title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>Inventory Management System</title>
 
     <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
