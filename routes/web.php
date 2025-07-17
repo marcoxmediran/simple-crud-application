f78f3d975b9a0f1240e6698da3277e8c2a81f762
marcoxmediran <marcoxmediran@gmail.com>
2025-07-17 12:52:39 +0800

    chore: add ProductController to routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/home');
+Route::redirect('/', '/product');
 
-Route::get('/home', function () {
-    return view('home');
-});
+Route::resource('/product', ProductController::class);
